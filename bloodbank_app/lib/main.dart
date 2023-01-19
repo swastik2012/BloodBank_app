6de28d33e315c16c6606a40c0a67a181b6a5fd17
swastik <swastikjuneja2012@gmail.com>
2023-01-19 09:29:09 +0530

    removed some spaces

--- bloodbank_app/lib/main.dart
+++ bloodbank_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Blood Bank App',
+      title: 'Bllood Bank App',
       home: const MyHomePage(),
     );
   }
